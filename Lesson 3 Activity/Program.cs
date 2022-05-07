f2f9c454a1959ce64e355b4f9f92c8e855650852
RMD00 <104664239+RMD00@users.noreply.github.com>
2022-05-07 16:07:28 +1000

    Lesson 3 Activity cleaned up and extensions added

--- Lesson 3 Activity/Program.cs
+++ Lesson 3 Activity/Program.cs
@@ -17,3 +17,24 @@ var monthsSavingFor = int.Parse(monthsSavingString);
 var savings = deposit * Math.Pow(1 + (interestRateAsPercentage/100),monthsSavingFor);
 Console.WriteLine("Your savings will be:");
 Console.WriteLine($"${savings}");
+
+// Extension activity
+Console.WriteLine("Would you like to save for longer? How many additional months?");
+var extraMonthsSavingString = Console.ReadLine();
+var extraMonthsSaving = int.Parse(extraMonthsSavingString);
+
+// This is the extra months result
+var extraMonths = (monthsSavingFor + extraMonthsSaving);
+var savings2 = (deposit * Math.Pow(1 + (interestRateAsPercentage/100),extraMonths)); 
+Console.WriteLine($"Saving for {extraMonths} months in total, means your saving will now be:");
+Console.WriteLine($"${savings2}");
+
+// This is the extra years result
+Console.WriteLine("Would you like to save for longer? How many years in total?");
+var yearsSavingString = Console.ReadLine();
+var yearsSavingFor = int.Parse(yearsSavingString);
+
+var yearsInMonths = (yearsSavingFor * 12);
+var savingsYears = deposit * Math.Pow(1 + (interestRateAsPercentage/100),yearsInMonths);
+Console.WriteLine($"Saving for {yearsSavingFor} years, means your savings will now be:");
+Console.WriteLine($"${savingsYears}");
