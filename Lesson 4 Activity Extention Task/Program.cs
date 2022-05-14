718d8776d4f0d05aafce864ee060d2581d9b6a5b
RMD00 <104664239+RMD00@users.noreply.github.com>
2022-05-14 16:55:36 +1000

    re-upload (didn't save code)

--- Lesson 4 Activity Extention Task/Program.cs
+++ Lesson 4 Activity Extention Task/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Part 1: Write an application that generates a number from 1 to 10
+// Extension task: Infinite Guesses
+
+Random rnd = new Random ();
+var randomNumber = rnd.Next(1,11);
+
+Console.WriteLine("What number am I thinking of between 1 and 10? You can keep guessing until you are correct.");
+var numberGuessed = Console.ReadLine();
+int numberGuessedAsInt = int.Parse(numberGuessed);
+Console.WriteLine($"You guessed: " + numberGuessedAsInt);
+if (randomNumber != numberGuessedAsInt)
+    {
+        Console.WriteLine("Sorry, that is not the number I am thinking of. Guess again...");
+    }
+else
+    {
+        Console.WriteLine("You are CORRECT! You WIN! YAAAAAY!");
+    }
+
+while (randomNumber != numberGuessedAsInt)
+    {
+        numberGuessed = Console.ReadLine();
+        Console.WriteLine($"You guessed: " + numberGuessedAsInt);
+        if (randomNumber == numberGuessedAsInt)
+            break;
+        Console.WriteLine("Sorry, that is not the number I am thinking of. Guess again...");
+    }
+
+Console.WriteLine("You are CORRECT! You WIN! YAAAAAY!");
