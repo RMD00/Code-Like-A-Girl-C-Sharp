efd8d2974f4c2ad589c3a39c4ec5344a4aa56bcf
RMD00 <104664239+RMD00@users.noreply.github.com>
2022-05-17 19:34:34 +1000

    Corrected While Loop

--- Lesson 4 Activity Extention Task/Program.cs
+++ Lesson 4 Activity Extention Task/Program.cs
@@ -4,26 +4,19 @@
 Random rnd = new Random ();
 var randomNumber = rnd.Next(1,11);
 
+Console.WriteLine(randomNumber);
 Console.WriteLine("What number am I thinking of between 1 and 10? You can keep guessing until you are correct.");
 var numberGuessed = Console.ReadLine();
 int numberGuessedAsInt = int.Parse(numberGuessed);
-Console.WriteLine($"You guessed: " + numberGuessedAsInt);
-if (randomNumber != numberGuessedAsInt)
-    {
-        Console.WriteLine("Sorry, that is not the number I am thinking of. Guess again...");
-    }
-else
-    {
-        Console.WriteLine("You are CORRECT! You WIN! YAAAAAY!");
-    }
 
 while (randomNumber != numberGuessedAsInt)
     {
-        numberGuessed = Console.ReadLine();
         Console.WriteLine($"You guessed: " + numberGuessedAsInt);
-        if (randomNumber == numberGuessedAsInt)
-            break;
         Console.WriteLine("Sorry, that is not the number I am thinking of. Guess again...");
+        numberGuessed = Console.ReadLine();
+        numberGuessedAsInt = int.Parse(numberGuessed);
+        if (randomNumber == numberGuessedAsInt)
+            break;        
     }
 
 Console.WriteLine("You are CORRECT! You WIN! YAAAAAY!");
