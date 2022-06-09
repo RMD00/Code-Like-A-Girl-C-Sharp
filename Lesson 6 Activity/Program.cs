b77c28f75f05b4d2b0f2a37b9de27b978eff8f72
RMD00 <104664239+RMD00@users.noreply.github.com>
2022-06-09 12:35:11 +1000

    No code errors, but still doesn't work

--- Lesson 6 Activity/Program.cs
+++ Lesson 6 Activity/Program.cs
@@ -1,29 +1,31 @@
 ﻿// Lesson 6 Acivity - Basic Calculator
+
 while (true)
 {
     Console.WriteLine("Please enter your first number");
     var firstNumber = Console.ReadLine();
-    var firstInteger = double.Parse(firstNumber); // how do I do the .IsNullOrEmpty?
+    var firstDouble = double.Parse(firstNumber); // how do I do the .IsNullOrEmpty?
 
     Console.WriteLine("Please enter an operation (+, /, raise to the power)");
     var chosenOperator = Console.ReadLine();
 
     Console.WriteLine("Please enter your second number");
     var secondNumber = Console.ReadLine();
-    var secondInteger = double.Parse(secondNumber); // how do I do the .IsNullOrEmpty?
+    var secondDouble = double.Parse(secondNumber); // how do I do the .IsNullOrEmpty?
 
     switch (chosenOperator)
         {
             case "+":
-            var result = Add(firstInteger, secondInteger);
+            var result = Add(firstDouble, secondDouble);
             break;
         }
+        
+    static extern double Add(double firstDouble, double secondDouble);
+    {
+        var add = firstDouble + secondDouble;
+        Console.WriteLine($"{firstDouble} + {secondDouble} = {add}");
+    }
 }
 
-double Add(double firstInteger, double secondInteger);
-    {
-        var add = firstInteger + secondInteger;
-        Console.WriteLine($"{firstInteger} + {secondInteger} = {add}");
-        return add;
-    }
+
 
