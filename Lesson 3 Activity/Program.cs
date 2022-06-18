eddeff1015bd4d94aa51639556265d8b6c7a9b00
RMD00 <104664239+RMD00@users.noreply.github.com>
2022-06-18 19:01:06 +1000

    Lesson 3 Activity: Cleaned & Complete
    Lesson 3: Operators
    Assignment: Savings Calculator
    Learning Objectives: Practice using operators | Practice using the Math class | Practice converting strings to numbers

--- Lesson 3 Activity/Program.cs
+++ Lesson 3 Activity/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Lesson 3: Savings Calculator
 
 // This is the data collection 
-Console.WriteLine("What is your initial deposit (please use numbers only?");
+Console.WriteLine("What is your initial deposit (please use numbers only)?");
 var depositString = Console.ReadLine();
 var deposit = int.Parse(depositString);
 
-Console.WriteLine("What is the monthly interest rate (please use numbers only?");
+Console.WriteLine("What is the monthly interest rate (please use numbers only)?");
 var interestRateString = Console.ReadLine();
 var interestRateAsPercentage = double.Parse(interestRateString);
 
-Console.WriteLine("How many months will you save for (please use numbers only?");
+Console.WriteLine("How many months will you save for (please use numbers only)?");
 var monthsSavingString = Console.ReadLine();
 var monthsSavingFor = int.Parse(monthsSavingString);
 
