58b86f59ab0bca88c85cf8e2cf0022fb9abcaca4
RMD00 <104664239+RMD00@users.noreply.github.com>
2022-06-18 18:43:49 +1000

    Lesson 1 Activity: Cleaned & Complete

--- Lesson 1 Activity/Program.cs
+++ Lesson 1 Activity/Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("So now we've begun upon our road");
+﻿Console.WriteLine("So now we've begun upon our road");
 Console.WriteLine("Of learning C#, as we learn to code");
